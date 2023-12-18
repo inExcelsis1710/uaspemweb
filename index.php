7fe0aae80b046c0a18aabaf531760ba6bdfd9615
121140037_Ignatius Krisna <116390512+inExcelsis1710@users.noreply.github.com>
2023-12-18 13:21:09 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Data Mahasiswa Universitas Fontaine</title>
+    <title>Data Mahasiswa Universitas Bootkemp Pemrograman Web</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <style>
         body {
@@ -303,7 +303,7 @@ function removeLocalStorage(key) {
 </body>
 
 <footer>
-    <p>&copy; UTS 1 PEMROGRAMAN WEB - IGNATIUS KRISNA </p>
+    <p>&copy; UAS PEMROGRAMAN WEB - IGNATIUS KRISNA </p>
 </footer>
 
 </html>
